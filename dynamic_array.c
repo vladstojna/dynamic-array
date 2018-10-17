591059314b1178bdf51a48a90a700da374e3a835
vladstojna <vlad.leic@gmail.com>
2018-10-17 15:52:34 +0100

    pop, queue, redone rm

--- dynamic_array.c
+++ dynamic_array.c
@@ -15,10 +15,10 @@
 /* Creates a new dynamic array with size sz */
 dyn_array *dynarr_new_n(size_t sz) {
 	dyn_array *arr;
-
+	/* alloc space */
 	arr       = (dyn_array*) malloc((size_t) sizeof(dyn_array));
 	arr->data = (dynarr_t*)  malloc((size_t) sz * sizeof(dynarr_t));
-
+	/* init variables */
 	arr->size   = sz;
 	arr->length = 0;
 	arr->first  = NULL;
@@ -91,15 +91,20 @@ void dynarr_push(dyn_array *dest, dynarr_t val) {
 		dest->data[dest->length] = val;
 	/* Else increase array size */
 	else {
-		size_t new_sz = sizeof(dynarr_t) * MULT * dest->size;
+		size_t    new_sz = sizeof(dynarr_t) * MULT * dest->size;
+		dynarr_t *data_new;
 		/* realloc */
-		dest->data = (dynarr_t*) realloc((dynarr_t*) dest->data, new_sz);
+		data_new = (dynarr_t*) realloc((dynarr_t*) dest->data, new_sz);
 
-		if (dest->data == NULL) {
+		if (data_new == NULL) {
+			/* free old */
+			dynarr_free(dest);
 			fprintf(stderr, "dynarr_append: realloc error\n");
 			exit(EXIT_FAILURE);
 		}
 
+		/* update vars */
+		dest->data = data_new;
 		dest->size *= MULT;
 		dest->data[dest->length] = val;
 	}
@@ -107,29 +112,44 @@ void dynarr_push(dyn_array *dest, dynarr_t val) {
 	dest->length += 1;
 }
 
+/* Enqueues a value to the array
+   Same as dynarr_push */
+void dynarr_enqueue(dyn_array *dest, dynarr_t val) {
+	dynarr_push(dest, val);
+}
+
 /* Appends buffer to array */
 void dynarr_append(dyn_array *dest, const dynarr_t *src, size_t buffsz) {
 	/* check if there is enough space */
 	if (dest->size - dest->length >= buffsz) {
 		if (memcpy(&dest->data[dest->length], src, sizeof(dynarr_t) * buffsz) == NULL) {
+			dynarr_free(dest);
 			fprintf(stderr, "dynarr_append: memcpy error\n");
 			exit(EXIT_FAILURE);
 		}
 	}
 	/* else allocate more memory then copy */
 	else {
-		size_t new_sz = sizeof(dynarr_t) * (buffsz + dest->length);
-		/* realloc */
-		dest->data = (dynarr_t*) realloc((dynarr_t*) dest->data, new_sz);
-		dest->size = new_sz;
+		size_t    new_sz = sizeof(dynarr_t) * (buffsz + dest->length);
+		dynarr_t *data_new;
 
-		if (dest->data == NULL) {
+		/* realloc */
+		data_new = (dynarr_t*) realloc((dynarr_t*) dest->data, new_sz);
+
+		if (data_new == NULL) {
+			/* free old */
+			dynarr_free(dest);
 			fprintf(stderr, "dynarr_append: realloc error\n");
 			exit(EXIT_FAILURE);
 		}
 
+		/* update vars */
+		dest->data = data_new;
+		dest->size = new_sz;
+
 		/* copy memory */
 		if (memcpy(&dest->data[dest->length], src, sizeof(dynarr_t) * buffsz) == NULL) {
+			dynarr_free(dest);
 			fprintf(stderr, "dynarr_append: memcpy error\n");
 			exit(EXIT_FAILURE);
 		}
@@ -142,48 +162,99 @@ void dynarr_append(dyn_array *dest, const dynarr_t *src, size_t buffsz) {
 void dynarr_trim(dyn_array *d) {
 	/* if not full */
 	if (d->length < d->size) {
-		size_t new_sz = sizeof(dynarr_t) * d->length;
+		size_t    new_sz = sizeof(dynarr_t) * d->length;
+		dynarr_t *data_new;
+
 		/* realloc */
-		d->data = (dynarr_t*) realloc((dynarr_t*) d->data, new_sz);
-		d->size = d->length;
+		data_new = (dynarr_t*) realloc((dynarr_t*) d->data, new_sz);
 
 		if (d->data == NULL) {
+			dynarr_free(d);
 			fprintf(stderr, "dynarr_append: realloc error\n");
 			exit(EXIT_FAILURE);
 		}
+
+		/* update vars */
+		d->data = data_new;
+		d->size = d->length;
 	}
 }
 
-/* Removes element from index i */
-void dynarr_rm(dyn_array *d, int i) {
-	if (i >= 0 && i < d->length) {
-		/* shifts data left after i */
-		for (int j = i; j < d->length - 1; j++)
-			d->data[j] = d->data[j+1];\
+/* -------------------------------------------------------------------------- */
+
+/* Pops from array */
+void dynarr_pop(dyn_array *d) {
+	dynarr_rmt(d, 0);
+}
+
+/* De-queue from array */
+void dynarr_dequeue(dyn_array *d) {
+	dynarr_rmt(d, d->length - 1);
+}
+
+/* Removes n elements from index i
+   Auto trims array when new length << size */
+void dynarr_rm_n(dyn_array *d, int i, int n) {
+
+	/* no removal needed */
+	if (n == 0) return;
+
+	/* args validity check */
+	if (i >= 0 && n > 0 && i < d->length) {
+
+		/* shift only if n is small enough
+		   shifts data left after i */
+		for ( ; i + n < d->length; i++)
+			d->data[i] = d->data[i+n];
+
 		/* updates length */
-		d->length -= 1;
+		d->length -= n;
 		/* trims when length is halved */
 		if (d->size >= MULT * d->length)
 			dynarr_trim(d);
 	}
+
 	else {
-		fprintf(stderr, "dynarr_get: index out of bounds %d\n", i);
+		dynarr_free(d);
+		fprintf(stderr, "dynarr_rm_n: invalid indices i=%d n=%d\n", i, n);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/* Removes element from index
+   Auto trims array when new length << size */
+void dynarr_rm(dyn_array *d, int i) {
+	dynarr_rm_n(d, i, 1);
+}
+
+/* Removes element from index and trims result */
+void dynarr_rmt_n(dyn_array *d, int i, int n) {
+
+	/* no removal needed */
+	if (n == 0) return;
+
+	/* args validity check */
+	if (i >= 0 && n > 0 && i < d->length) {
+
+		/* shift only if n is small enough
+		   shifts data left after i */
+		for ( ; i + n < d->length; i++)
+			d->data[i] = d->data[i+n];
+
+		/* updates length */
+		d->length -= n;
+		/* trims */
+		dynarr_trim(d);
+	}
+
+	else {
+		dynarr_free(d);
+		fprintf(stderr, "dynarr_rm_n: invalid indices i=%d n=%d\n", i, n);
 		exit(EXIT_FAILURE);
 	}
 }
 
 /* Removes element from index and trims result */
 void dynarr_rmt(dyn_array *d, int i) {
-	if (i >= 0 && i < d->length) {
-		/* shifts data left after i */
-		for (int j = i; j < d->length - 1; j++)
-			d->data[j] = d->data[j+1];\
-		/* updates length */
-		d->length -= 1;
-		dynarr_trim(d);
-	}
-	else {
-		fprintf(stderr, "dynarr_get: invalid index\n");
-		exit(EXIT_FAILURE);
-	}	
+	dynarr_rmt_n(d, i, 1);
 }
--- dynamic_array.h
+++ dynamic_array.h
@@ -11,13 +11,13 @@
 
 /* Struct definition */
 typedef struct dynamic_array_t {
-	/* total size */
-	size_t    size;
-	/* used size */
-	size_t    length;
-	/* first useful element */
+	/* size   - total size
+	   length - used size
+	   first  - first useful 
+	   last   - last useful  */
+	size_t size;
+	size_t length;
 	dynarr_t *first;
-	/* last useful element */
 	dynarr_t *last;
 	/* data array */
 	dynarr_t *data;
@@ -61,11 +61,9 @@ void dynarr_set(dyn_array *d, int i, dynarr_t val);
 /* Pushes a value to the end of the array */
 void dynarr_push(dyn_array *dest, dynarr_t val);
 
-/* TODO Pops from array */
-void dynarr_pop(dyn_array *d);
-
-/* TODO De-queue from array */
-void dynarr_dequeue(dyn_array *d);
+/* Enqueues a value to the array
+   Same as dynarr_push */
+void dynarr_enqueue(dyn_array *dest, dynarr_t val);
 
 /* Appends buffer to array */
 void dynarr_append(dyn_array *dest, const dynarr_t *src, size_t buffsz);
@@ -73,9 +71,25 @@ void dynarr_append(dyn_array *dest, const dynarr_t *src, size_t buffsz);
 /* Trims array to useful size */
 void dynarr_trim(dyn_array *dest);
 
+/* -------------------------------------------------------------------------- */
+
+/* Pops from array */
+void dynarr_pop(dyn_array *d);
+
+/* De-queue from array */
+void dynarr_dequeue(dyn_array *d);
+
+/* Removes n elements from index i
+   Auto trims array when new length << size */
+void dynarr_rm_n(dyn_array *d, int i, int n);
+
 /* Removes element from index
    Auto trims array when new length << size */
 void dynarr_rm(dyn_array *d, int i);
+
+/* Removes n elements from index i
+   Always trims result */
+void dynarr_rmt_n(dyn_array *d, int f, int l);
 
 /* Removes element from index and trims result */
 void dynarr_rmt(dyn_array *d, int i);
